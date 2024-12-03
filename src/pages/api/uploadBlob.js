993c772a192660e0eae7af61b28a8e7207d97d11
Olli Salo <ullebror@gmail.com>
2024-12-03 13:21:57 +0200

    removed the parser stuff

--- src/pages/api/uploadBlob.js
+++ src/pages/api/uploadBlob.js
@@ -39,9 +39,3 @@ export default async function handler(req, res) {
 
     //return res.status(200).json({ url: blob.url });
 }
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
